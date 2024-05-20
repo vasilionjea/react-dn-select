f608af052534a106b3ea18be2c6426b20775d2d7
Billy Onjea <vasili.onjea@gmail.com>
2024-05-19 22:35:57 -0700

    update example

--- example/names.ts
+++ example/names.ts
@@ -1,7 +1,7 @@
 const genStrings = (size: number) => {
   return Array(size)
     .fill('')
-    .map((_, i) => (Math.random() * (i + 1) * 10).toString(36).slice(3, 7));
+    .map((_, i) => (Math.random() * (i + 1) * 10).toString(36).slice(4, 8));
 };
 
-export const names = genStrings(130);
+export const names = genStrings(120);
